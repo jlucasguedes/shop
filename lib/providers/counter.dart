e13ede357f9dcf4057f8bdd7a8972ab2b3d7b4fa
João Lucas Guedes de Oliveira <iftolucas@gmail.com>
2022-10-12 06:36:31 -0300

    Gerenciamento de Estado

---
+++ lib/providers/counter.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CounterState {
+  int _value = 0;
+
+  void inc() => _value++;
+  void dec() => _value--;
+  int get value => _value;
+
+  bool diff(CounterState old) {
+    return old._value != _value;
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final CounterState state = CounterState();
+
+  CounterProvider({required Widget child, Key? key})
+      : super(child: child, key: key);
+
+  static CounterProvider? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CounterProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant CounterProvider oldWidget) {
+    return oldWidget.state.diff(state);
+  }
+}
